0ab5fec9512fd63de4b62572a98ad941c0774301
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2022-09-21 13:10:58 +0200

    created a base for the game

--- TextBasedAdventure.py
+++ TextBasedAdventure.py
@@ -4,24 +4,33 @@ import time
 from time import sleep
 import sys
 import random
+from turtle import color
 
-def scrollTxt(text):
-   for char in text:
+def scrollTxt(text, color):
+    print(colorDict[color], end="")
+    for char in text:
        sys.stdout.write(char)
        sys.stdout.flush()
        time.sleep(0.035)
+    input("")
 
-
+#don't forget: 
+#change the '0;' to '1;' for bold text 
 colorDict = {
     "black" : "\033[0;30m",
     "purple" : "\033[0;35m",
     "blue" : "\033[0;34m",
     "green" : "\033[0;32m",
+    "lightGreen" : "\033[1;32m",
     "red" : "\033[0;31m",
     "yellow" : "\033[0;33m",
     "white" : "\033[0;37m"
 }
 
-print(colorDict["blue"])
+# print(colorDict["green"])
 
-scrollTxt("qwesrdgyjk;.qwertgjkl,;awsfdfhgh")
+scrollTxt("red text", "red")
+scrollTxt("white text", "white")
+scrollTxt("yellow text", "yellow")
+
+print(colorDict["white"])
